4914080754d1f2cd3473b7ad3cb7d0b033dcfd8a
Rushikesh Gaikwad <rushikeshhtc@gmail.com>
2024-02-05 15:27:47 +0530

    simple change

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with Rushi
+          Learn React with Rushi Anil Gaikwad
         </a>
       </header>
     </div>
