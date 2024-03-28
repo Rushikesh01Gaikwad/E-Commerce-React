d65d35a2321c0f7a99c5a450be4c6b4ebd251ba9
Rushikesh Gaikwad <rushikeshhtc@gmail.com>
2024-03-28 15:09:27 +0530

    working on search

--- src/header/Navbar.js
+++ src/header/Navbar.js
@@ -1,61 +1,69 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import logo from "../Images/logo/mitraalogoo.png";
 import { useSelector } from "react-redux";
-import articlejson from '../articles.json'
+import articlejson from '../articles.json';
+import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { mainCard } from "../slices/Maincardslice";
 
 export default function Navbar() {
 
-  const [searchTerm, setSerchTerm] = useState("")
+  const dispatch = useDispatch();
 
-  const cartItems = useSelector(state => state.cart)
+  const [searchTerm, setSearchTerm] = useState("");
+  const cartItems = useSelector(state => state.cart);
+  const [searchResultsVisible, setSearchResultsVisible] = useState(true);
+
+  const handleResultClick = () => {
+    setSearchResultsVisible(false); // Close search results when link is clicked
+  };
+
 
   return (
     <div>
-      <nav
-        className="navbar navbar-expand-lg"
-        style={{ backgroundColor: "#0074eb" }}>
+      <nav className="navbar navbar-expand-lg" style={{ backgroundColor: "#0074eb" }}>
         <div className="container-fluid">
           <a className="navbar-brand">
             <img className="complogo my-3" src={logo} alt=""></img>
           </a>
           <div className="dropdown mx-1">
-              <button
-                className="btn custom-button dropdown-toggle btn-light my-3"
-                type="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="true"
-              >
-                Select Country
-              </button>
-              <ul class="dropdown-menu">
-                <li>
-                  <a className="dropdown-item" href="/">
-                    India
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="/">
-                    USA
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="/">
-                    Canada
-                  </a>
-                </li>
-              </ul>
-            </div>
+            <button
+              className="btn custom-button dropdown-toggle btn-light my-3"
+              type="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="true"
+            >
+              Select Country
+            </button>
+            <ul className="dropdown-menu">
+              <li>
+                <a className="dropdown-item" href="/">
+                  India
+                </a>
+              </li>
+              <li>
+                <a className="dropdown-item" href="/">
+                  USA
+                </a>
+              </li>
+              <li>
+                <a className="dropdown-item" href="/">
+                  Canada
+                </a>
+              </li>
+            </ul>
+          </div>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <input
-                className="form-control mx-2"
-                id="input"
-                placeholder="Search here"
-                rows="1"
-                onChange={(event)=>{
-                  setSerchTerm(event.target.value);
-                }}
+              className="form-control mx-2"
+              id="input"
+              placeholder="Search here"
+              rows="1"
+              onChange={(event) => {
+                setSearchTerm(event.target.value);
+              }}
             />
-              </div>
+          </div>
           <button
             type="button"
             className="btn btn-light mx-2 my-3"
@@ -83,33 +91,53 @@ export default function Navbar() {
             aria-controls="offcanvasWithBothOptions"
           >
             🛒Cart
-            <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill text-bg-warning">
-            {cartItems.length}  
-              <span class="visually-hidden"></span>
+            <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill text-bg-warning">
+              {cartItems.length}
+              <span className="visually-hidden"></span>
             </span>
           </button>
         </div>
       </nav>
-      <div className="template_Container">
-              {
-                articlejson 
+      <div>
+        <div className="container">
+            <div className="container mt-3 searchDiv">
+              <div className="row">
+                {searchResultsVisible && articlejson
                   .filter((val) => {
-                    if(searchTerm === ""){
-                      return null
-                    }else if(val.title.toLowerCase().includes(searchTerm.toLowerCase())){
+                    if (searchTerm === "") {
+                      return null;
+                    } else if (val.title.toLowerCase().includes(searchTerm.toLowerCase())) {
                       return val;
                     }
                   })
                   .map((val) => {
-                    return(
-                      <div className="template" key={val.id}>
-                          <h3>{val.title}</h3>
-                          <h3>{val.price}</h3>
-                      </div> 
-                    )
-                  })
-              }
+                    return (
+                      <div className="col-md-3 mb-3" key={val.id}>
+                         <Link to="/Mainproduct" onClick={handleResultClick}>
+                          <div className="card"  onClick={() =>
+                              dispatch(
+                                mainCard({
+                                  name: val.title,
+                                  price: val.price,
+                                  image: val.image,
+                                  description: val.description,
+                                  rate: val.rating.rate,
+                                })
+                              )
+                            }>
+                            <div className="card-body">
+                              <h5 className="card-title">{val.title}</h5>
+                              <p className="card-text">{val.price}</p>
+                            </div>
+                          </div>
+                        </Link>
+                      </div>
+                    );
+                  })}
+              </div>
             </div>
+        </div>
+      </div>
     </div>
   );
 }
