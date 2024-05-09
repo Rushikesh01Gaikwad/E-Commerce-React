bf89715dea28ee44275de77cd1ff2f55d7cd4996
Rushikesh Gaikwad <rushikesh.gaikwad@external.atlascopco.com>
2024-05-09 16:07:59 +0530

    registere user completed

--- src/popups/SignupPopup.js
+++ src/popups/SignupPopup.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import Box from '@mui/material/Box';
 import axios from 'axios';
-import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import doneGif from '../DoneMassage.gif';
 
 export default function MultilineTextFields() {
 
@@ -15,7 +13,7 @@ export default function MultilineTextFields() {
   const [city, setCity] = useState('');
   const [state, setState] = useState('');
   const [pin, setPin] = useState('');
-  
+
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -29,7 +27,7 @@ export default function MultilineTextFields() {
         state,
         pin
       });
-      
+
       console.log('Post added:', response.data);
       // Optionally, you can redirect or show a success message here
     } catch (error) {
@@ -39,88 +37,65 @@ export default function MultilineTextFields() {
   };
 
   return (
-    <div className='signupcontainer container'>
-      <form onSubmit={handleSubmit}>
-        <Box
-          sx={{
-            '& .MuiTextField-root': { m: 4, width: '25ch' },
-          }}
-          noValidate
-          autoComplete="off"
-        >
-          <div>
-            <TextField
-              type="text"
-              id="name"
-              value={name}
-              label="Enter Name"
-              multiline
-              maxRows={4}
-              variant="standard"
-              onChange={(e) => setName(e.target.value)}  
-            />
-            <TextField
-              id="email"
-              value={email}
-              label="Enter Email"
-              multiline
-              maxRows={4}
-              variant="standard"
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <TextField
-              id="mobile"
-              value={mobile}
-              label="Mobile Number"
-              multiline
-              maxRows={4}
-              variant="standard"
-              onChange={(e)=> setMobile(e.target.value)}
-            />
-            <TextField
-              id="address"
-              value={address}
-              label="Address"
-              multiline
-              rows={4} // corrected attribute name
-              variant="standard"
-              onChange={(e) => setAddress(e.target.value)}
-            />
+    <div>
+      <div className='signupcontainer container'>
+        <form onSubmit={handleSubmit}>
+
+          <div className='signupInput'>
+            <div class="mb-3 sizeadjustdivmaincard mt-2">
+              <label for="name" class="form-label">Full Name</label>
+              <input type="text" class="form-control" id="name" onChange={(e) => setName(e.target.value)} />
+            </div>
+            <div class="mb-3 sizeadjustdivmaincard mt-2" style={{ marginLeft: "1vw" }}>
+              <label for="mobile" class="form-label">Mobile Number</label>
+              <input type="text" class="form-control" id="mobile" onChange={(e) => setMobile(e.target.value)} />
+            </div>
           </div>
-          <div>
-            <TextField
-              id="city"
-              value={city}
-              label="City"
-              multiline
-              maxRows={4}
-              variant="standard"
-              onChange={(e) => setCity(e.target.value)}
-            />
-            <TextField
-              id="state"
-              value={state}
-              label="State"
-              multiline
-              maxRows={4}
-              variant="standard"
-              onChange={(e)=> setState(e.target.value)}
-            />
-            <TextField
-              id="pin"
-              value={pin}
-              label="Pin code"
-              multiline
-              maxRows={4}
-              variant="standard"
-              onChange={(e)=> setPin(e.target.value)}
-            />
-            <Button variant="contained" size="large" type="submit">
-              <b>Submit</b>
-            </Button>
+          <div className='signupInput'>
+            <div class="mb-3 sizeadjustdivmaincard">
+              <label for="email" class="form-label">Email address</label>
+              <input type="text" class="form-control" id="email" onChange={(e) => setEmail(e.target.value)} />
+            </div>
+            <div class="mb-3 sizeadjustdivmaincard" style={{ marginLeft: "1vw" }}>
+              <label for="city" class="form-label">City</label>
+              <input type="text" class="form-control" id="city" onChange={(e) => setCity(e.target.value)} />
+            </div>
           </div>
-        </Box>
-      </form>
+          <div class="mb-3">
+            <label for="address" class="form-label">Address</label>
+            <textarea class="form-control" id="address" rows="3" onChange={(e) => setAddress(e.target.value)}></textarea>
+          </div>
+          <div className='signupInput'>
+            <div class="mb-3 sizeadjustdivmaincard">
+              <label for="state" class="form-label">State</label>
+              <input type="text" class="form-control" id="state" onChange={(e) => setState(e.target.value)} />
+            </div>
+            <div class="mb-3 sizeadjustdivmaincard" style={{ marginLeft: "1vw" }}>
+              <label for="pin" class="form-label">Pin Code</label>
+              <input type="text" class="form-control" id="pin" onChange={(e) => setPin(e.target.value)} />
+            </div>
+          </div>
+          <Button variant="contained" size="large" type="submit" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+            <b>Submit</b>
+          </Button>
+        </form>
+      </div>
+      
+      <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h1 class="modal-title fs-5" id="staticBackdropLabel">User Added Successfully</h1>
+            </div>
+            <div class="modal-body">
+              <img src={doneGif} alt='done_img' width="465"/>
+            </div>
+            <div class="modal-footer">
+              <Button variant="contained" size="large" data-bs-dismiss="modal" data-bs-toggle="modal" data-bs-target="#staticBackdropsignin"><b>Login</b></Button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
