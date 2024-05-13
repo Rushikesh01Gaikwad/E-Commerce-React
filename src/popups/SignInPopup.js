2a75f30962c49c1bafe08677136d631daeeda016
Rushikesh Gaikwad <rushikeshhtc@gmail.com>
2024-05-13 15:10:40 +0530

    sign up in progress

--- src/popups/SignInPopup.js
+++ src/popups/SignInPopup.js
@@ -1,43 +1,121 @@
-import React from 'react';
-import usericon from '../Images/icons/usericon.png';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import usericon from "../Images/icons/usericon.png";
+import { Link, useNavigate } from "react-router-dom";
+import Button from "@mui/material/Button";
 
 export default function SignInPopup() {
+  const [eml, setEmail] = useState("");
+  const [pass, setPass] = useState("");
+
+  const usenavigate = useNavigate();
+
+  const proceedLogin = (e) => {
+    e.preventDefault();
+    if (validate()) {
+      console.log("proceed")
+      fetch('http://localhost:3000/Data?'+eml).then((res)=>{
+        return res.json();
+      }).then((resp)=>{
+        console.log(resp)
+        if (Object.keys(resp).length === 0){
+          console.log("Enter valid email")
+        }
+        else{
+          if(resp.mobile === pass){
+            console.log("login successfully")
+            //usenavigate('/')
+          } 
+          else{
+            console.log("Enter valid credential")
+          }
+        }
+      }).catch((err)=>{
+        console.log("login failed due to "+err.message);
+      })
+    }
+  };
+  const validate = () => {
+    let result = true;
+    if (eml === "" || eml === null) {
+      result = false;
+      console.log("enter the username");
+    }
+    if (pass === "" || pass === null) {
+      result = false;
+      console.log("enter the password");
+    }
+    return result
+  };
+
   return (
-    <div className="modal fade" id="staticBackdropsignin" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div
+      className="modal fade"
+      id="staticBackdropsignin"
+      data-bs-backdrop="static"
+      data-bs-keyboard="false"
+      tabIndex="-1"
+      aria-labelledby="staticBackdropLabel"
+      aria-hidden="true"
+    >
       <div className="modal-dialog modal-dialog-centered">
         <div className="modal-content">
           <div className="modal-header bg-light">
             <h5 className="modal-title text-primary">Sign In</h5>
-            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            <button
+              type="button"
+              className="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
           </div>
           <div className="modal-body">
             <div className="text-center mb-4">
               <img src={usericon} className="usericonlogo" alt="Login user" />
             </div>
-            <form>
+            <form onSubmit={proceedLogin}>
               <div className="mb-3">
-                <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter your email address" />
+                <label htmlFor="exampleInputEmail1" className="form-label">
+                  Email address
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="exampleInputEmail1"
+                  aria-describedby="emailHelp"
+                  placeholder="Enter your email address"
+                  value={eml}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
               </div>
               <div className="mb-3">
-                <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Enter your password" />
-              </div>
-              <div className="mb-3 form-check">
-                <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                <label className="form-check-label" htmlFor="exampleCheck1">Remember me</label>
+                <label htmlFor="exampleInputPassword1" className="form-label">
+                  Mobile{" "}
+                </label>
+                <input
+                  type="password"
+                  className="form-control"
+                  id="exampleInputPassword1"
+                  placeholder="Enter your Mobile Number"
+                  value={pass}
+                  onChange={(e) => setPass(e.target.value)}
+                />
               </div>
               <div className="d-grid gap-2">
-                <button type="button" className="btn btn-primary btn-lg">Sign In</button>
+                <Button type="submit" variant="contained">
+                  <b>Sign In</b>
+                </Button>
               </div>
             </form>
           </div>
           <div className="modal-footer justify-content-between bg-light">
             <Link to="/signup">
-              <button type="button" className="btn btn-link text-primary" data-bs-dismiss="modal">Sign Up</button>
+              <Button type="button" variant="contained" data-bs-dismiss="modal">
+                Sign Up
+              </Button>
             </Link>
-            <button type="button" className="btn btn-link text-secondary" data-bs-dismiss="modal">Close</button>
+            <Button type="button" variant="contained" data-bs-dismiss="modal">
+              Close
+            </Button>
           </div>
         </div>
       </div>
--- src/popups/SignupPopup.js
+++ src/popups/SignupPopup.js
@@ -2,7 +2,8 @@ import React from 'react';
 import axios from 'axios';
 import Button from '@mui/material/Button';
 import { useState } from 'react';
-import doneGif from '../DoneMassage.gif';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function MultilineTextFields() {
 
@@ -13,7 +14,6 @@ export default function MultilineTextFields() {
   const [city, setCity] = useState('');
   const [state, setState] = useState('');
   const [pin, setPin] = useState('');
-
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -29,11 +29,36 @@ export default function MultilineTextFields() {
       });
 
       console.log('Post added:', response.data);
-      // Optionally, you can redirect or show a success message here
+      notify(); // Notify success
+      resetForm(); // Reset form fields
+
     } catch (error) {
       console.error('Error adding post:', error);
       // Handle error, show error message, etc.
     }
+  };
+
+  const resetForm = () => {
+    setName('');
+    setEmail('');
+    setMobile('');
+    setAddress('');
+    setCity('');
+    setState('');
+    setPin('');
+  };
+
+  const notify = () => {
+    toast.success("User added successfully", {
+      position: "bottom-right",
+      autoClose: 5000,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+    });
   };
 
   return (
@@ -44,58 +69,43 @@ export default function MultilineTextFields() {
           <div className='signupInput'>
             <div class="mb-3 sizeadjustdivmaincard mt-2">
               <label for="name" class="form-label">Full Name</label>
-              <input type="text" class="form-control" id="name" onChange={(e) => setName(e.target.value)} />
+              <input type="text" class="form-control" id="name" value={name} onChange={(e) => setName(e.target.value)} />
             </div>
             <div class="mb-3 sizeadjustdivmaincard mt-2" style={{ marginLeft: "1vw" }}>
               <label for="mobile" class="form-label">Mobile Number</label>
-              <input type="text" class="form-control" id="mobile" onChange={(e) => setMobile(e.target.value)} />
+              <input type="text" class="form-control" id="mobile" value={mobile} onChange={(e) => setMobile(e.target.value)} />
             </div>
           </div>
           <div className='signupInput'>
             <div class="mb-3 sizeadjustdivmaincard">
               <label for="email" class="form-label">Email address</label>
-              <input type="text" class="form-control" id="email" onChange={(e) => setEmail(e.target.value)} />
+              <input type="text" class="form-control" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
             </div>
             <div class="mb-3 sizeadjustdivmaincard" style={{ marginLeft: "1vw" }}>
               <label for="city" class="form-label">City</label>
-              <input type="text" class="form-control" id="city" onChange={(e) => setCity(e.target.value)} />
+              <input type="text" class="form-control" id="city" value={city} onChange={(e) => setCity(e.target.value)} />
             </div>
           </div>
           <div class="mb-3">
             <label for="address" class="form-label">Address</label>
-            <textarea class="form-control" id="address" rows="3" onChange={(e) => setAddress(e.target.value)}></textarea>
+            <textarea class="form-control" id="address" rows="3" value={address} onChange={(e) => setAddress(e.target.value)}></textarea>
           </div>
           <div className='signupInput'>
             <div class="mb-3 sizeadjustdivmaincard">
               <label for="state" class="form-label">State</label>
-              <input type="text" class="form-control" id="state" onChange={(e) => setState(e.target.value)} />
+              <input type="text" class="form-control" id="state" value={state} onChange={(e) => setState(e.target.value)} />
             </div>
             <div class="mb-3 sizeadjustdivmaincard" style={{ marginLeft: "1vw" }}>
               <label for="pin" class="form-label">Pin Code</label>
-              <input type="text" class="form-control" id="pin" onChange={(e) => setPin(e.target.value)} />
+              <input type="text" class="form-control" id="pin" value={pin} onChange={(e) => setPin(e.target.value)} />
             </div>
           </div>
-          <Button variant="contained" size="large" type="submit" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+          <Button variant="contained" size="large" type="submit" data-bs-toggle="modal" data-bs-target="#staticBackdropsignin">
             <b>Submit</b>
           </Button>
         </form>
       </div>
-      
-      <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h1 class="modal-title fs-5" id="staticBackdropLabel">User Added Successfully</h1>
-            </div>
-            <div class="modal-body">
-              <img src={doneGif} alt='done_img' width="465"/>
-            </div>
-            <div class="modal-footer">
-              <Button variant="contained" size="large" data-bs-dismiss="modal" data-bs-toggle="modal" data-bs-target="#staticBackdropsignin"><b>Login</b></Button>
-            </div>
-          </div>
-        </div>
-      </div>
+      <ToastContainer />
     </div>
   );
 }
